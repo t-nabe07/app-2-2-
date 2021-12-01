79523b2b04f8cbf770154ee171bbbbf46003f7f4
t-nabe07 <watanabe.t.job@gmail.com>
2021-12-01 10:33:05 +0000

    [Update]app-3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,8 +4,11 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    if comment.save
+      redirect_to book_path(book)
+    else
+      render 'book/show'
+    end
   end
 
   def destroy
@@ -13,7 +16,9 @@ class BookCommentsController < ApplicationController
     redirect_to book_path(params[:book_id])
   end
 
+
   private
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
