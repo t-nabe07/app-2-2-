8ec71149ef55125cd7676fcd99c6584a3eb7c60e
t-nabe07 <watanabe.t.job@gmail.com>
2021-12-04 05:16:13 +0000

    [Start]app-5

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update] do
-    #member do
-      #get :follows, :followers
-    #end
-    #resource :relationships, only: [:create,:destroy]
     resource :relationships, only: [:create, :destroy]
     get :followings, on: :member
     get :followers, on: :member
