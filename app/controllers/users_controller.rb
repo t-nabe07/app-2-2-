5651fb6e90ad5a080b16e45a26b2b5fc62b7c3ca
t-nabe07 <watanabe.t.job@gmail.com>
2021-12-06 08:14:24 +0000

    [Update]app-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    #@count_book = Book.find(params[:id])
   end
 
   def index
