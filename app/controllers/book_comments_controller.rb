4a744034e3979e9bb8bbb5b84b7cb46e6269fd56
t-nabe07 <watanabe.t.job@gmail.com>
2021-12-07 02:55:04 +0000

    [Update]app-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,11 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      render 'create'
     else
       redirect_to request.referer
     end
